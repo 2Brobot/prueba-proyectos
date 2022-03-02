bba7907cc3e38c3647d89237a06153537146c1d1
2Brobot <3003939970101@ingenieria.edu.gt>
2022-03-01 19:21:21 -0600

    cambio del iva de 0.12 a 0.14

--- problema3m.m
+++ problema3m.m
@@ -1,6 +1,6 @@
 pkg load database 
 conn = pq_connect(setdbopts('dbname','postgres','host','localhost','port','5432','user','postgres','password','123'))
 costo = input('Introduce el costo: ');
-iva=costo*0.12
-sin_iva=costo*0.88
+iva=costo*0.14
+sin_iva=costo*0.86
 N=pq_exec_params(conn, "insert into pexamen3 (costo, iva, sin_iva) values($1, $2, $3);",{costo,iva,sin_iva})
